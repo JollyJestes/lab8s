545867f615b07db7077ee3591975e5bf238c2876
JollyJestes <93829971+JollyJestes@users.noreply.github.com>
2023-11-15 17:50:33 +0200

    Add project files.

---
+++ lab8s/DataSerializer.cs
@@ -0,0 +1,69 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+
+public class DataSerializer<T>
+{
+    public void SerializeBinary(T data, string fileName)
+    {
+        try
+        {
+            using (FileStream fs = new FileStream(fileName, FileMode.Create))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(fs, data);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при сериализации в бинарном формате: {ex.Message}");
+        }
+    }
+
+    public T DeserializeBinary(string fileName)
+    {
+        try
+        {
+            using (FileStream fs = new FileStream(fileName, FileMode.Open))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                return (T)formatter.Deserialize(fs);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при десериализации из бинарного формата: {ex.Message}");
+            return default;
+        }
+    }
+
+    public void SerializeJson(T data, string fileName)
+    {
+        try
+        {
+            string jsonString = JsonSerializer.Serialize(data, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+            File.WriteAllText(fileName, jsonString);
+            Console.WriteLine($"Данные успешно сохранены в JSON файл: {fileName}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при сериализации в JSON: {ex.Message}");
+        }
+    }
+
+    public T DeserializeJson(string fileName)
+    {
+        try
+        {
+            string jsonString = File.ReadAllText(fileName);
+            return JsonSerializer.Deserialize<T>(jsonString);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при десериализации из JSON: {ex.Message}");
+            return default;
+        }
+    }
+}
---
+++ lab8s/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+
+[Serializable]
+public class Movie
+{
+    public string Title { get; set; }
+    public int Watched { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var films = new List<Movie>();
+
+        // Ввод фильмов с клавиатуры
+        while (true)
+        {
+            Console.WriteLine("Введите название фильма (или 'q' для завершения):");
+            string userInput = Console.ReadLine();
+
+            if (userInput.ToLower() == "q")
+                break;
+
+            var film = new Movie { Title = userInput, Watched = GetRandomWatchedStatus() };
+            films.Add(film);
+        }
+
+        // Создание объекта для сериализации/десериализации данных
+        var dataSerializer = new DataSerializer<List<Movie>>();
+
+        // Сохранение данных в бинарном файле
+        dataSerializer.SerializeBinary(films, "movies.dat");
+
+        // Восстановление данных из бинарного файла
+        var restoredMoviesBinary = dataSerializer.DeserializeBinary("movies.dat");
+
+        // Вывод восстановленных данных из бинарного файла
+        Console.WriteLine("\nДанные после восстановления из бинарного файла:");
+        PrintMovies(restoredMoviesBinary);
+
+        // Сохранение данных в JSON-файле
+        dataSerializer.SerializeJson(films, "movies.json");
+
+        // Восстановление данных из JSON-файла
+        var restoredMoviesJson = dataSerializer.DeserializeJson("movies.json");
+
+        // Вывод восстановленных данных из JSON-файла
+        Console.WriteLine("\nДанные после восстановления из JSON-файла:");
+        PrintMovies(restoredMoviesJson);
+    }
+
+    static int GetRandomWatchedStatus()
+    {
+        // Генерация случайного числа 0 или 1 для статуса просмотра
+        Random random = new Random();
+        return random.Next(0, 2);
+    }
+
+    static void PrintMovies(IEnumerable<Movie> movies)
+    {
+        foreach (var movie in movies)
+        {
+            Console.WriteLine($"{movie.Title} - Смотрел: {(movie.Watched == 1 ? "Да" : "Нет")}");
+        }
+    }
+}
